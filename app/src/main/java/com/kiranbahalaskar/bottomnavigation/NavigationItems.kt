c3668b5d7db3d6b10927c328da0adbe2e3bbfc17
Kiran <kbahalaskar@rediffmail.com>
2021-10-01 15:31:40 +0530

    From Kiran

---
+++ app/src/main/java/com/kiranbahalaskar/bottomnavigation/NavigationItems.kt
@@ -0,0 +1,8 @@
+package com.kiranbahalaskar.bottomnavigation
+
+sealed class NavigationItems(var route: String, var icon: Int, var title: String)
+{
+    object Home : NavigationItems("home", R.drawable.ic_home, "Home")
+    object Profile : NavigationItems("profile", R.drawable.ic_profile, "Profile")
+    object Settings : NavigationItems("settings", R.drawable.ic_settings, "Settings")
+}
